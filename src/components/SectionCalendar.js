30605eba7dd87b25b30df9f0b3f7cbdd149f7c20
Tom Claes <tom.claes@cegeka.com>
2020-03-20 20:03:54 +0100

    refactor calendar section

--- src/components/SectionCalendar.js
+++ src/components/SectionCalendar.js
@@ -5,16 +5,36 @@ import {htmlToReact, markdownify} from '../utils';
 
 export default class SectionCalendar extends React.Component {
     render() {
-        let section = _.get(this.props, 'section');
+      let section = _.get(this.props, 'section');
 
-        const iframe = `
-            <iframe 
-               src="https://www.google.com/calendar/embed?height=600&amp;wkst=2&amp;bgcolor=%23FFFFFF&amp;src=buidtelberg.be_i9hmmqncntaa8lq15k35htnltg%40group.calendar.google.com&amp;color=%23F4511E&amp;ctz=Europe%2FBrussels"
-              style="border: 0"
-              width="800" 
-              height="600" 
-            >                  
-            </iframe>`
+      /*
+        Check Google calendar options to adjust
+        Use %23 instead of # for color
+      */
+
+      const calendarOptions = {
+        weekStart: '2',
+        showTitle: false,
+        bgColor: '%23FFFFFF',
+        color: '%23F4511E',
+        calendarSrc: 'buidtelberg.be_i9hmmqncntaa8lq15k35htnltg%40group.calendar.google.com',
+        timeZone: 'Europe%2FBrussels',
+        showTimeZone: false,
+        borderStyle: '0'
+      };
+
+      const src = `
+          https://www.google.com/calendar/embed?height=600&amp;wkst=${calendarOptions.weekStart}&amp;showTitle=${calendarOptions.showTitle}&amp;bgcolor=${calendarOptions.bgColor}&amp;color=${calendarOptions.color}&amp;src=${calendarOptions.calendarSrc}&amp;ctz=${calendarOptions.timeZone}&amp;showTz=${calendarOptions.showTimeZone}
+        `;
+
+      const iframe = `
+        <iframe 
+          src=${src}
+          style='border: ${calendarOptions.borderStyle}'
+          width="800" 
+          height="600" 
+        >                  
+        </iframe>`
 
       return (
             <section id={_.get(section, 'section_id')} className={'block contact-block bg-' + _.get(section, 'bg') + ' outer'}>
@@ -38,5 +58,5 @@ export default class SectionCalendar extends React.Component {
 }
 
 function Iframe(props) {
-  return (<div class={props.class} dangerouslySetInnerHTML={ {__html:  props.iframe?props.iframe:""}} />);
+  return (<div class={props.class?props.class:""} dangerouslySetInnerHTML={ {__html:  props.iframe?props.iframe:""}} />);
 }
