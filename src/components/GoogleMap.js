54470634a767ccb8fb9c7fa79d1c080c09c8d7fe
Tom Claes <tom.claes@cegeka.com>
2020-03-20 20:55:31 +0100

    gmaps added

--- src/components/GoogleMap.js
+++ src/components/GoogleMap.js
@@ -6,20 +6,34 @@ import {markdownify} from "../utils";
 export class MapContainer extends React.Component {
   render() {
     let section = _.get(this.props, 'section');
+
+    const style = {
+      width: '80%',
+      height: '100%',
+      maxWidth: '800px',
+      position: 'relative',
+    };
+
     return (
-      <section id={_.get(this.props, 'section.section_id')} className="block cta-block bg-accent outer">
-        <div className="block-content inner-medium">
-          {markdownify(_.get(section, 'content'))}
+      <section id={_.get(this.props, 'section.section_id')} className="block text-block outer">
+        <div className="inner">
+          <div className="block-content inner-medium">
+            {markdownify(_.get(section, 'content'))}
+          </div>
+          <div className="block maps">
+            <Map
+              google={this.props.google}
+              zoom={14}
+              style={style}
+              initialCenter={{
+                lat: 51.023577,
+                lng: 5.394429
+              }}
+            >
+              <Marker name={'Current location'} />
+            </Map>
+          </div>
         </div>
-        <Map 
-          google={this.props.google} zoom={14}
-          initialCenter={{ 
-            lat: 51.023577, 
-            lng: 5.394429
-          }}
-        >
-          <Marker name={'Current location'} />
-        </Map>
       </section>
     );
   }
